64252950443f8a2fa048780aa7f54f6adc5af86a
diegoantoniodeoliveira <diegoantoniodeoliveira@hotmail.com>
2024-05-06 09:26:45 -0300

    footer

--- projetoVielasSQ/src/components/Footer/index.jsx
+++ projetoVielasSQ/src/components/Footer/index.jsx
@@ -1,22 +1,31 @@
 import styles from'./styles.module.css'
+import { TiSocialFacebookCircular } from "react-icons/ti";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+
 
 export const Footer = () =>  {
   return (
 
     <div className={styles.container}>
           <div className={styles.vielas}> 
-                  <h3>VIELAS</h3>
-                  </div>
-        
-            <div className={styles.icon}>
-            <a>icones</a>
-            </div>
-            <div className={styles.texto1}>
+            <h3>VIELAS</h3>
+              <div className={styles.icon}>
+                <a><TiSocialFacebookCircular className={styles.icons}/></a>
+                <a><FaInstagram className={styles.icons}/></a>
+                <a><FaTwitter className={styles.icons}/></a>
+                <a><FaWhatsapp className={styles.icons}/></a>
+                <a><FaLinkedinIn className={styles.icons}/></a>
+              </div>
+          </div>
+               <div className={styles.textos1}>
                  <p>Quem Somos</p>
                  <p>Equipe</p>
-                 <p>Apoie</p>
+                 <p>poie</p>
                  <p>Fale Conosco</p>
-                </div>
+               </div>
     </div>
   )
 }
